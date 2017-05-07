a31eecacde5195075d73229629e9dcd8c2c14537
佳期 <js_bin@163.com>
2017-05-07 14:53:28 +0800

    1.引入webpack模块别名逻辑

--- webpack-env/base.config.js
+++ webpack-env/base.config.js
@@ -4,16 +4,17 @@
 var path = require('path');
 var webpack = require('webpack');
 //定义了一些文件夹的路径
-var ROOT_PATH = path.resolve(__dirname,'..');
+var ROOT_PATH = path.resolve(__dirname, '..');
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var CleanPlugin = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-module.exports = {
+var aliasPath = require(path.resolve(ROOT_PATH, 'app/alias.config.js'));
+var config = {
     //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
     entry: {
-        app :APP_PATH,
+        app: APP_PATH,
         vendor: ['lodash']
     },
     //输出的文件名 合并以后的js会命名为bundle.js
@@ -39,16 +40,27 @@ module.exports = {
         }),
         new ExtractTextPlugin('main.[hash:8].css'),
     ],
-    module:{
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({fallback:'style-loader', use:'css-loader'})
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)\??.*$/,
-                loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
-            }
-        ]
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader'
+            })
+        },
+        {
+            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)\??.*$/,
+            loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
+        }]
     }
 };
+if (aliasPath) { //模块别名
+    for (var key in aliasPath) {
+        aliasPath[key] = path.join(APP_PATH, aliasPath[key]);
+    }
+    config.resolve = {
+        modules: [APP_PATH, path.resolve(ROOT_PATH, 'node_modules')],
+        alias: aliasPath
+    };
+}
+module.exports = config;
