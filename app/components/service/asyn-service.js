300b081f760ba92f9c0601d7b47a7b77012f2ce2
282712 <282712@nd.com>
2017-06-22 21:58:09 +0800

    加入webpack异步import() 方式支持

---
+++ app/components/service/asyn-service.js
@@ -0,0 +1,5 @@
+import isString from 'lodash/isString';
+
+export function isEmptyString(str) {
+    return str && isString(str);
+}
--- app/index.js
+++ app/index.js
@@ -3,7 +3,12 @@ import 'bootstrap-css-only';
 import './theme/css/style.css';
 import './components/mock/mock-init.js';
 
-import dataService from  'dataService';
+import dataService from 'dataService';
 
 dataService.addValue('test',121113);
 console.log(dataService.getValue('test'));
+
+// 异步 chunk 设置
+import('./components/service/asyn-service').then(function ({ isEmptyString }) {
+    console.log(isEmptyString('str'), isEmptyString(''));
+});
