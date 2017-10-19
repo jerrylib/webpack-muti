b3c0e5d2bb11bd93f3fd0a97e393fdbde989b30d
李彬 <180505@nd.com>
2017-10-19 15:27:22 +0800

    chore(*):删除部分无效的引用包

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'test/**/*.spec.js': ['webpack']
+            'test/**/*.spec.js': ['webpack'],
         },
         // test results reporter to use
         // possible values: 'dots', 'progress'
