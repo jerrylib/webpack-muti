953aed246d0c9ff120a3e784aded0bb24facf420
李彬 <180505@nd.com>
2017-10-25 19:22:58 +0800

    doc(_arraySerivce):添加signArray方法注释

--- src/_array/_arrayService.js
+++ src/_array/_arrayService.js
@@ -2,7 +2,7 @@
  * @Author: libin 
  * @Date: 2017-10-25 15:41:12 
  * @Last Modified by: libin
- * @Last Modified time: 2017-10-25 19:10:30
+ * @Last Modified time: 2017-10-25 19:22:30
  */
 import _ from 'lodash';
 const defaults = {
@@ -13,7 +13,16 @@ const defaults = {
   // 选中时的标识字段
   signColumn: 'checked'
 };
-
+/**
+ * 标识基础列表上选中的条目
+ * @param {*} bases 基础列表 
+ * @param {*} signs 被选中列表
+ * @param {*} options 配置项
+ * @return {
+ *  list:被处理过的基础列表
+ *  unMaplist:被选中列表中的条目，未出现在基础列表中的。
+ * }
+ */
 function signArray(bases, signs, options) {
   //设置默认配置项
   options = _.assign({}, defaults, options);
